7d9b1e5a8f32f9451ca6d7335bb6a2d30869fe0c
Albin Halitaj <albin.halitaj@gmail.com>
2022-03-28 22:23:09 +0100

    added tests

---
+++ AzureApp.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace AzureApp.Tests.Controllers
+{
+    using AzureApp.Controllers;
+    using Microsoft.AspNetCore.Mvc;
+    using Xunit;
+
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void HomeController_ShouldReturn_View()
+        {
+            // arrange
+            var controller = new HomeController();
+
+            // act 
+            var result = controller.Index();
+
+            //asset
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
